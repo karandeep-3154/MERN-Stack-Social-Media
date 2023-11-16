827b4cfc139edab81a5d16854b9fdef66c3d742b
karandeep-3154 <ksingh310504@gmail.com>
2023-11-16 20:40:35 +0530

    1st Modal Implementation and Integration Completed

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { MantineProvider } from '@mantine/core';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useMantineTheme } from '@mantine/core';
+// const theme = createTheme({
+//   /** Your theme override here */fontFamily: 'Open Sans, sans-serif',
+//   primaryColor: 'cyan',
+// });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   {/* <MantineProvider  theme={theme} > */}
+      <App />
+    {/* </MantineProvider> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
